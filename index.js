3bb638b2bd0123a0983b53534e83e6b6398eaa4d
Jason Arritt <jsn.arritt@gmail.com>
2022-04-08 11:48:21 -0400

    Organized files and added comments for info to collect

---
+++ index.js
@@ -0,0 +1,36 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require ('./utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+const questions = [
+
+// Collect project title
+
+// Collect description information
+
+// MAKE SURE TO INCLUDE TABLE OF CONTENTS IN MARKDOWN
+
+// Collect installation information
+
+// Collect usage information
+
+// Collect license information
+
+// Collect contribution information
+
+// Collect testing information
+
+// Collect email for questions/contact
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
